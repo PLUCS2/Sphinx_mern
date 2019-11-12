d299c6f70eb38e532fb2f6701a98b86649d00218
PLUCS2 <psecunda@elon.edu>
2019-11-12 14:59:32 -0500

    Add users post route

--- app.js
+++ app.js
@@ -7,7 +7,17 @@ mongoose
   .then(() => console.log("Connected to MongoDB successfully"))
   .catch(err => console.log(err));
 
-app.get("/", (req, res) => res.send("Hello Fele!"));
+  const users = require("./routes/api/users");
+  const prophecies = require("./routes/api/prophecies");
+  const bodyParser = require("body-parser");
+  
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.get("/", (req, res) => res.send("Hello Seekers!"));
+app.use("/api/users", users);
+app.use("/api/prophecies", prophecies);
+
 
 const port = process.env.PORT || 5000;
 
