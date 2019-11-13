64faa87e29908a478eea854456b71bd86571b034
PLUCS2 <psecunda@elon.edu>
2019-11-13 16:12:47 -0500

    Configure for Heroku

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ const prophecies = require("./routes/api/prophecies");
 const bodyParser = require("body-parser");
 
 const passport = require("passport");
+const path = require("path");
   
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
@@ -23,6 +24,12 @@ require("./config/passport")(passport);
 app.use("/api/users", users);
 app.use("/api/prophecies", prophecies);
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("frontend/build"));
+  app.get("/", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
+  });
+}
 
 const port = process.env.PORT || 5000;
 
